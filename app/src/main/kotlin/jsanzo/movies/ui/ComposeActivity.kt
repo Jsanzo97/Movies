cfe6454b939bfe6a17aadff2301567c412177f8d
Jorge Sanzo Hernando <jorgesanzo21@gmail.com>
2026-02-23 20:09:39 +0100

    [Task - 20] Remove AppRemoteModule and and Chucker-no-op for release (#11)

--- app/src/main/kotlin/jsanzo/movies/ui/ComposeActivity.kt
+++ app/src/main/kotlin/jsanzo/movies/ui/ComposeActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.LaunchedEffect
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
+import jsanzo.movies.BuildConfig
 import jsanzo.movies.ui.navigation.AppNavigation
 import jsanzo.movies.ui.theme.MoviesTheme
 
@@ -29,7 +30,7 @@ class ComposeActivity : ComponentActivity() {
     @OptIn(ExperimentalPermissionsApi::class)
     @Composable
     fun RequestNotificationPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        if (BuildConfig.DEBUG && Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             val permissionState = rememberPermissionState(
                 permission = Manifest.permission.POST_NOTIFICATIONS,
             )
